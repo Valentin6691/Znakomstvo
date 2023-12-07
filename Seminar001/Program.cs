253b4a4461d118fd8df0784171c25479a5c6d883
Valentin <nitnelav6691@gmail.cov>
2023-12-07 15:25:59 +0300

    Seminar с проверкой

--- Seminar001/Program.cs
+++ Seminar001/Program.cs
@@ -1,6 +1,13 @@
 ﻿Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
-int number1 = number/100;
+if(number>99 && number<1000)
+{int number1 = number/100;
 int number3 = number % 10;
 int result = number1*10 + number3;
 Console.WriteLine(result);
+}
+else
+{
+    
+    Console.WriteLine("Введите трехначное число");
+}
