55896c698eb02bffa5edc25e0c93e2f22fa3dbeb
Valentin <nitnelav6691@gmail.cov>
2023-12-07 17:18:40 +0300

    Seminar004

---
+++ Seminar004/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число1");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        int number2 = number1/100;
+        int number3 = number2%10;
+        if(number1>99)
+        {
+           Console.WriteLine(number3); 
+        }
+        else
+        {
+            Console.WriteLine("Число имеет меньше трех знаков");
+        }
