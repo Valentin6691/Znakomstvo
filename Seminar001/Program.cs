4de499f8094a998b92668d70c8297e3560951ff8
Valentin <nitnelav6691@gmail.cov>
2023-12-07 15:02:36 +0300

    Seminar001

---
+++ Seminar001/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int number1 = number/100;
+int number3 = number % 10;
+int result = number1*10 + number3;
+Console.WriteLine(result);
