7cd3854ce7f94a7d9342fea486359047b90f7796
Valentin <nitnelav6691@gmail.cov>
2023-12-07 18:32:59 +0300

    Homeork001

---
+++ Homework001/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int num = Convert.ToInt32(Console.ReadLine());
+        if (num % 7 == 0 && num % 23 == 0)
+        {
+            Console.WriteLine("Да");
+        }
+        else
+        {
+            Console.WriteLine("Нет");
+        }
+    }
+}
