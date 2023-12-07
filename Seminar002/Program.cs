4412a0466eba9bc8a24837f0a2e35a79fe849a42
Valentin <nitnelav6691@gmail.cov>
2023-12-07 16:14:47 +0300

    Seminar002

---
+++ Seminar002/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number > 99 && number < 1000)
+        {
+            int number2 = number / 10 % 10;
+            int number3 = number % 10;
+            int result = number2;
+            while (number3 > 1)
+            {
+                result = result * number2;
+                number3--;
+            }
+            Console.WriteLine(result);
+        }
+        else
+        {
+
+            Console.WriteLine("Введите трехначное число");
+        }
+    }
+}
