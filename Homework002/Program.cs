d2f1f59d44bfb76552120616f3e0d818c813f9b3
Valentin <nitnelav6691@gmail.cov>
2023-12-07 19:02:41 +0300

    Homeork002

---
+++ Homework002/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите абсциссу");
+        int abs = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите ординату");
+        int ord = Convert.ToInt32(Console.ReadLine());
+        if (abs == 0 && ord == 0)
+        {
+            Console.WriteLine("Одна или две координаты равны 0");
+        }
+            else if (abs > 0 && ord > 0)
+            {
+                Console.WriteLine("1");
+            }
+            else if (abs < 0 && ord > 0)
+            {
+                Console.WriteLine("2");
+            }
+            else if (abs < 0 && ord < 0)
+            {
+                Console.WriteLine("3");
+            }
+            else if (abs > 0 && ord < 0)
+            {
+                Console.WriteLine("4");
+            }
+    }
+}
