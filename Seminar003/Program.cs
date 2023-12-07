81ccdac2e010c106790da37d656ac1f4c9753448
Valentin <nitnelav6691@gmail.cov>
2023-12-07 16:50:44 +0300

    Seminar003

---
+++ Seminar003/Program.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число1");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число2");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        int result = number1 % number2;
+        if (number1 > number2)
+        {
+            if (result == 0)
+            {
+                Console.WriteLine("Да");
+            }
+            else
+            {
+                Console.WriteLine("Нет, " + number1 % number2);
+            }
+
+        }
+        else
+        {
+            Console.WriteLine("Числа не подходят");
+        }
+    }
+}
